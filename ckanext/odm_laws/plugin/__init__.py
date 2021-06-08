05f591128d51156a1dbffd3e511ef043ad0415c9
gtkChop <swaroopbhatk@gmail.com>
2021-06-08 10:36:59 +0000

    remove unwanted code

--- ckanext/odm_laws/plugin/__init__.py
+++ ckanext/odm_laws/plugin/__init__.py
@@ -23,45 +23,6 @@ else:
   from ckanext.odm_laws.plugin.pylons_plugin import OdmLawsMixinPlugin
 
 
-def _create_or_update_pdf_thumbnail(context,pkg_dict_or_resource):
-  pdf_url=pkg_dict_or_resource['url']
-  filename, file_extension = os.path.splitext(pdf_url)
-
-  if ".pdf" != file_extension.lower() or pkg_dict_or_resource['name'] == "PDF Thumbnail":
-   return
-
-  enabled_pdf_preview = False
-  # enabled_pdf_preview = config.get("ckan.odm_nav_concept.generate_pdf_preview", False) == "true"
-  if enabled_pdf_preview:
-
-    try:
-      from wand.image import Image
-
-      pdf=Image(filename=pdf_url+"[0]")
-      pdf.format='png'
-      pdf.resize(135,201)
-      temp_dir = os.path.abspath(tempfile.mkdtemp())
-      temp_img=temp_dir+'/'+pkg_dict_or_resource['id']+'.png'
-      pdf.save(filename=temp_img)
-      params = {'package_id':pkg_dict_or_resource['package_id'],'upload':temp_img, 'url':'N/A','format':'PNG','mimetype_inner':'image/png','name':'PDF Thumbnail'}
-      ckan_url = config.get("ckan.site_url", "")
-      userobj = context['auth_user_obj']
-      ckan_auth = userobj.apikey
-
-      if context['resource'].name == "PDF Thumbnail":
-        resource_id=context['resource'].id
-        params['id']=resource_id
-        requests.post(ckan_url + 'api/3/action/resource_update',verify=False,data=params,headers={"X-CKAN-API-Key": ckan_auth},files=[('upload', file(params["upload"]))])
-      else:
-        requests.post(ckan_url + 'api/3/action/resource_create',verify=False,data=params,headers={"X-CKAN-API-Key": ckan_auth},files=[('upload', file(params["upload"]))])
-
-      if os.path.exists(temp_img):
-        os.remove(temp_img)
-
-    except Exception, e:
-      log.error("Could not generate PDF thumbnail", e)
-
-
 class OdmLawsPlugin(OdmLawsMixinPlugin):
   '''OD Mekong laws plugin.'''
 
@@ -95,15 +56,3 @@ class OdmLawsPlugin(OdmLawsMixinPlugin):
       if review_system:
         if 'type' in pkg_dict_or_resource:
           odm_laws_helper.create_default_issue_laws_record(pkg_dict_or_resource)
-
-        if 'url_type' in pkg_dict_or_resource:
-          _create_or_update_pdf_thumbnail(context,pkg_dict_or_resource)
-
-  def after_update(self, context, pkg_dict_or_resource):
-    log.debug('after_update: %s', pkg_dict_or_resource['name'])
-
-    dataset_type = context['package'].type if 'package' in context else pkg_dict_or_resource['type']
-    if dataset_type == 'laws_record':
-
-      if 'url_type' in pkg_dict_or_resource:
-        _create_or_update_pdf_thumbnail(context,pkg_dict_or_resource)
